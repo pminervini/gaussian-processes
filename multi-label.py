6ba42b9f7ccc8fa572eb158d3a2f5ebcc7524813
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2016-02-22 22:20:09 +0100

    update

--- multi-label.py
+++ multi-label.py
@@ -15,8 +15,11 @@ def energy(F, l, Y, W, C, mu, eps, eta):
     rL = (np.diag(W.sum(axis=0)) - W) + eps * np.eye(W.shape[0])
     Es = (F.T.dot(rL.dot(F))).trace()
 
-    E = El + mu * Es
 
+    L_C = - (np.diag(C.sum(axis=0)) - C)
+    Ec = - (F.dot(L_C.dot(F.T))).trace()
+
+    E = El + (mu * Es) + (eta * Ec)
     return E
 
 
@@ -24,12 +27,13 @@ def propagate(l, Y, W, C, mu, eps, eta):
     S = np.diag(l)
     rL = (np.diag(W.sum(axis=0)) - W) + eps * np.eye(W.shape[0])
 
-    _A = (S + mu * rL)
+    n, k = Y.shape[0], Y.shape[1]
 
-    k = Y.shape[1]
-    A = np.kron(_A, np.eye(k))
+    L_C = - (np.diag(C.sum(axis=0)) - C)
+    A = np.kron((S + mu * rL), np.eye(k)) - eta * np.kron(np.eye(n), L_C)
 
     b = (S.dot(Y)).flatten()
+
     F_star = np.linalg.solve(A, b)
 
     return F_star.reshape(Y.shape)
@@ -39,34 +43,32 @@ def main(argv):
     np.random.seed(0)
 
     for _ in range(2 ** 18):
+        n, k = 4, 2
+        mu, eps, eta = np.random.rand(), np.random.rand(), np.random.rand()
 
-        n, k = 32, 16
-        mu, eps = np.random.rand(), np.random.rand()
+        random_matrix_W = np.random.rand(n, n)
+        random_matrix_C = np.random.rand(k, k)
 
-        random_matrix = np.random.rand(n, n)
-        W = random_matrix + random_matrix.T
+        W = random_matrix_W + random_matrix_W.T
+        C = random_matrix_C + random_matrix_C.T
 
         Y = np.random.randint(2, size=(n, k))
         l = np.random.randint(2, size=n)
 
-        F_star = propagate(l, Y, W, None, mu, eps, None)
-        lowest_energy_value = energy(F_star, l, Y, W, None, mu, eps, None)
+        F_star = propagate(l, Y, W, C, mu, eps, eta)
+        lowest_energy_value = energy(F_star, l, Y, W, C, mu, eps, eta)
 
         noise = np.random.normal(loc=.0, scale=1e-2, size=(n, k))
         corrupted_F_star = F_star + noise
 
-        energy_value = energy(corrupted_F_star, l, Y, W, None, mu, eps, None)
+        energy_value = energy(corrupted_F_star, l, Y, W, C, mu, eps, eta)
+
         logging.debug('%s < %s' % (lowest_energy_value, energy_value))
 
-        assert(lowest_energy_value < energy_value)
+        assert(lowest_energy_value <= energy_value)
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     main(sys.argv[1:])
-
-
-
-
-
 
